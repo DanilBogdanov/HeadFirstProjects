a8101823096ad58a9380ff8196221bd5edd1ce3b
DanilKardon <dangerdanil@gmail.com>
2021-06-22 23:25:04 +0500

    Добавьте файлы проекта.

---
+++ SimpleForTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleForTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
