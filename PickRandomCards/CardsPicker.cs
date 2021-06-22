510b063cb10d0addf1e7da7b2fedfd136615f811
DanilKardon <dangerdanil@gmail.com>
2021-06-23 00:05:58 +0500

    add project "PickRandomCards"

---
+++ PickRandomCards/CardsPicker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class CardsPicker
+    {
+        static Random random = new();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+            return value switch
+            {
+                1 => "Spades",
+                2 => "Hearts",
+                3 => "Clubs",
+                _ => "Diamond",
+            };
+        }
+
+        static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            return value switch
+            {
+                1 => "Ace",
+                11 => "Jack",
+                12 => "Queen",
+                13 => "King",
+                _ => value.ToString()
+            };
+        }
+    }
+}
