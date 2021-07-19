9940f9092c0ea2a126ada67417446ea1cfead60b
Danil HP <bogdanovdanila1404@gmail.com>
2021-07-20 00:37:28 +0500

    add SwordDamageWPF

---
+++ SwordDamageWpf/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SwordDamageWpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ SwordDamageWpf/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SwordDamageWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
