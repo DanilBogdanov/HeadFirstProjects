b4ef903dbe59b198b7965f4106ea807502443784
Danil HP <bogdanovdanila1404@gmail.com>
2021-07-22 00:13:36 +0500

    finish HiLo

--- Hi-Lo/HiLoGame.cs
+++ Hi-Lo/HiLoGame.cs
@@ -3,41 +3,51 @@ using System.Runtime.CompilerServices;
 
 namespace Hi_Lo
 {
-    /*5. A method called Guess with a bool parameter called higher
-that does the following (look closely at the Main method to see
-how it’s called):
-It picks the next random number for the player to guess.
-If the player guessed higher and the next number is >=
-the current number OR if the player guessed lower and
-the next number is <= the current number, write “You
-guessed right!” to the console and increment the pot.
-Otherwise, write “Bad luck, you guessed wrong.” to
-the console and decrement the pot.
-Replace the current number with the one chosen at the
-beginning of the method and writes “The current
-number is” followed by the number to the console.
-6. A method called Hint that finds half the maximum, then writes
-either “The number is at least {half}” or “The number is at most
-{half}” to the console and decrements the pot.*/
+    
     public static class HiLoGame
     {
         public const int MAXIMUM = 10;
         private static Random random = new Random();
-        private static int currentNumber = random.Next(0, MAXIMUM);
+        private static int currentNumber = random.Next(1, MAXIMUM + 1);
         private static int pot = MAXIMUM;
 
         public static void Guess(bool higher)
         {
-            
+            int nextNumber = random.Next(0, MAXIMUM + 1);
+            if ((higher && nextNumber >= currentNumber)
+                || (!higher && nextNumber <= currentNumber))
+            {
+                Console.WriteLine("You guessed right!");
+                pot++;
+            }
+            else
+            {
+                Console.WriteLine("Bad luck, you guessed wrong.");
+                pot--;
+            }
+
+            currentNumber = nextNumber;
+            Console.WriteLine($"The current number is {currentNumber}");
         }
 
         public static int GetPot()
         {
             return pot;
         }
+
         public static void Hint()
         {
-            
+            int half = MAXIMUM / 2;
+            if (currentNumber <= half)
+            {
+                Console.WriteLine($"The number is at least {half}");
+            }
+            else
+            {
+                Console.WriteLine($"The number is at most {half}");
+            }
+
+            pot--;
         }
     }
 }
