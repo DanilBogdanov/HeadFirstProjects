4a230d833c781793ca88d0169584e3c2b1b9f14d
Danil Work Win7 <Dangerdanil@gmail.com>
2021-07-21 16:55:34 +0500

    add behavior of SwordDamageWpf

--- SwordDamageWpf/MainWindow.xaml.cs
+++ SwordDamageWpf/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using SwordDamage_ConsoleApp;
 
 namespace SwordDamageWpf
 {
@@ -7,34 +10,57 @@ namespace SwordDamageWpf
     /// </summary>
     public partial class MainWindow
     {
+        Random random = new Random();
+        SwordDamage swordDamage = new SwordDamage();
+
         public MainWindow()
         {
             InitializeComponent();
+            swordDamage.SetFlaming(false);
+            swordDamage.SetMagic(false);
+            
+        }
+
+        void RollDice()
+        {
+            swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            swordDamage.SetFlaming(flaming.IsChecked.Value);
+            swordDamage.SetMagic(magic.IsChecked.Value);
+            DisplayDamage();
+        }
+
+        void DisplayDamage()
+        {
+            damage.Text = "Rolled " + swordDamage.Roll + "for " + swordDamage.Damage + " HP";
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            RollDice();
         }
 
         private void Flaming_Checked(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            swordDamage.SetFlaming(true);
+            DisplayDamage();
         }
 
         private void Flaming_Unchecked(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            swordDamage.SetFlaming(false);
+            DisplayDamage();
         }
 
         private void Magic_Checked(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            swordDamage.SetMagic(true);
+            DisplayDamage();
         }
 
         private void Magic_Unchecked(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            swordDamage.SetMagic(false);
+            DisplayDamage();
         }
     }
 }
---
+++ SwordDamageWpf/SwordDamage.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+namespace SwordDamage_ConsoleApp
+{
+    public class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        private decimal MagicMultiplier = 1M;
+        private int FlamingDamage = 0;
+        public int Damage;
+
+        private void CalculateDamage()
+        {
+            Damage = (int) (Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+            Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage();
+            Debug.WriteLine($"SetMagic finished: {Damage} (roll: {Roll})");
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {            
+            if (isFlaming)
+            {
+                FlamingDamage = FLAME_DAMAGE;
+            }
+            else
+            {
+                FlamingDamage = 0;
+            }
+            CalculateDamage();
+            Debug.WriteLine($"SetFlaming finished: {Damage} (roll: {Roll})");
+        }
+    }
+}
