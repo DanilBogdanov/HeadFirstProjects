55f9a74b25dce2b33fd0f60bfdc5dea89dc40062
Danil Work Win7 <Dangerdanil@gmail.com>
2021-09-09 16:29:56 +0500

    finish large project

--- TwoDecksWPF/Deck.cs
+++ TwoDecksWPF/Deck.cs
@@ -9,6 +9,53 @@ namespace TwoDecksWPF
 {
     internal class Deck : ObservableCollection<Card>
     {
+        private static Random random = new Random();
 
+        public Deck()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            Clear();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 1; j <= 13; j++)
+                {
+                    Add(new Card((Suit)i, (Value)j));
+                }
+            }
+        }
+
+        public Card Deal(int index)
+        {
+            return null;
+        }
+
+        public void Shuffle()
+        {
+            List<Card> cards = new List<Card>(this);
+            Clear();
+            
+            while (cards.Count > 0)
+            {
+                int item = random.Next(cards.Count);
+                Add(cards[item]);
+                cards.RemoveAt(item);
+            }
+        }
+
+        public void Sort()
+        {
+            List<Card> sortedCards = new List<Card>(this);
+            sortedCards.Sort(new CardComparerByValue());
+            Clear();
+
+            foreach (Card card in sortedCards)
+            {
+                Add(card);
+            }
+        }
     }
 }
